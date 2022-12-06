2773ea7462c3c1fdb9a40f6171dd0d275771b48a
Jose <jose@josellausas.com>
2022-12-05 21:51:10 -0600

    Updated tests.py

--- tests.py
+++ tests.py
@@ -3,6 +3,9 @@ import environ
 import os
 from unittest import mock
 
+from llaulib.chato import Chato
+from llaulib.chato import Notapi
+
 env = environ.Env(
     DEBUG=(bool, True),
     SLACK_TOKEN = (str, 'unknown_key'),
@@ -19,7 +22,7 @@ mockEnv = {
 class TestLlauLibChato(unittest.TestCase):
     
     def test_chato(self):
-        from llaulib.chato import Chato
+        
         self.assertIsNotNone(Chato.getInstance())
 
         # Should Post
@@ -35,12 +38,10 @@ class TestLlauLibChato(unittest.TestCase):
 class TestLlauLibNotapi(unittest.TestCase):
     @mock.patch.dict(os.environ, mockEnv, clear=True)
     def test_napi(self):
-        from llaulib.chato import Notapi
         self.assertIsNotNone(Notapi)
     
     @mock.patch.dict(os.environ, mockEnv, clear=True)
     def test_is_domain_live(self):
-        from llaulib.chato import Notapi
         self.assertTrue(Notapi.is_domain_live('llau.systems'))
 
 if __name__ == '__main__':
